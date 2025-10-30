829674c46a29aead254ed0727213b2c889113482
Albert <albertjokro22@gmail.com>
2025-10-30 22:22:58 +0700

    merge fix

--- config/constant.ts
+++ config/constant.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const WHY_US = [
 
 ]
@@ -10,17 +9,4 @@ export const VISI_MISI = [
 	"Membangun kolaborasi strategis antara regulator, klien, dan praktisi untuk memperkuat kepercayaan publik.",
 	"Mengembangkan sistem manajemen internal yang menjunjung integritas dan akuntabilitas.",
 	"Mendukung percepatan literasi risiko, asuransi, dan sertifikasi melalui edukasi strategis dan platform digital.",
-=======
-const WHY_US = [
-
-]
-
-export const VISI_MISI = [
-	"Merancang dan mengimplementasikan strategi konsultasi asuransi berbasis data dan tata kelola risiko progresif.",
-	"Menghadirkan solusi sertifikasi yang taat regulasi, sekaligus mendorong reputasi dan kredibilitas industri.",
-	"Menjadi mitra terpercaya dalam layanan legalitas usaha (CV & PT) dengan proses cepat, legal, dan profesional.",
-	"Membangun kolaborasi strategis antara regulator, klien, dan praktisi untuk memperkuat kepercayaan publik.",
-	"Mengembangkan sistem manajemen internal yang menjunjung integritas dan akuntabilitas.",
-	"Mendukung percepatan literasi risiko, asuransi, dan sertifikasi melalui edukasi strategis dan platform digital.",
->>>>>>> a8a64de0cba7e7466a4639333e051926b9114276
 ];
