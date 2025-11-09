ebe1408e1bc521a4f523ef055a9f387c419c5224
Albert <albertjokro22@gmail.com>
2025-11-09 16:45:31 +0700

    eslint/compat fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,8 @@ import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
